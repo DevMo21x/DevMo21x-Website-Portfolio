8156da1d670b418f6167aaf1bc67de3111a64abc
DevMo21x <devmo.21x@outlook.com>
2026-08-15 18:31:29 -0300

    fix: set site and base in astro config for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://devmo21x.github.io',
+  base: '/DevMo21x-Website-Portfolio',
   integrations: [tailwind(), react()],
 });
